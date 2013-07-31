5cd763943b8f84c84099434ff53a0b3508e635fe
Philipp <philipp.nehaev@gmail.com>
2013-08-01 00:14:26 +0400

    Initial commit

---
+++ require-handlebars.js
@@ -0,0 +1,35 @@
+define(['lib/text', 'lib/handlebars'], function (text, Handlebars) {
+
+    var buildMap = {};
+
+    return {
+
+        load: function(name, parentRequire, onload, config) {
+
+            text.get(
+                parentRequire.toUrl(name + '.hbs'),
+                function (data) {
+
+                    if (config.isBuild) {
+
+                        buildMap[name] = Handlebars.precompile(data);
+                    }
+
+                    onload(Handlebars.compile(data));
+                },
+                onload.error
+            );
+        },
+
+        write: function(pluginName, moduleName, write) {
+
+            if (moduleName in buildMap) {
+
+                var templateSpec = buildMap[moduleName];
+
+                write('define("' + pluginName + '!' + moduleName + '",' +
+                    'function(){ return Handlebars.VM.template(' + templateSpec + ');});\n');
+            }
+        }
+    };
+});
